7b2fef235b0d0ae82e0a31f49d56ce2c97e99cfa
Abdulai059 <abdulaiosman8080@gmail.com>
2025-09-26 21:33:11 +0000

    first commit

--- src/ui/Logo.jsx
+++ src/ui/Logo.jsx
@@ -12,9 +12,14 @@ const Img = styled.img`
 function Logo() {
   return (
     <StyledLogo>
-      <Img src="/logo-light.png" alt="Logo" />
+      <Img src="./public/logo-light.png" alt="Logo" />
     </StyledLogo>
   );
 }
 
 export default Logo;
+
+
+git add .
+git commit -m "first commit"
+git push
--- src/ui/MainNav.jsx
+++ src/ui/MainNav.jsx
@@ -44,3 +44,16 @@ const Link = styled.a`
     color: var(--color-brand-600);
   }
 `;
+
+
+
+
+function MainNav() {
+  return (
+    <nav>
+      Nav
+    </nav>
+  )
+}
+
+export default MainNav
