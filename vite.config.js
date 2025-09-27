f128a83ea1e2cadd53436b33bbefdde05703c4ab
Abdulai059 <abdulaiosman8080@gmail.com>
2025-09-27 20:52:44 +0000

    first commit

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,19 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import eslint from "vite-plugin-eslint";
-import path from "path"; 
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), eslint()],
+  plugins: [
+    react(),
+    eslint({
+      eslintPath: "eslint",
+      overrideConfigFile: ".eslintrc.json", 
+    }),
+  ],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      "@": "/src",
     },
   },
 });
