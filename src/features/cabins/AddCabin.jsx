bb0887d053269e0a77e290a2be57203e2d456aef
Abdulai059 <abdulaiosman8080@gmail.com>
2025-10-09 16:00:02 +0000

    Converting the modal to Compound Componet

--- src/features/cabins/AddCabin.jsx
+++ src/features/cabins/AddCabin.jsx
@@ -4,25 +4,40 @@ import Button from "../../ui/Button";
 import Modal from "../../ui/Modal";
 
 function AddCabin() {
-  const [isOpenModal, setIsOpenModal] = useState(false);
-
   return (
-    <div>
-      <Button
-        $variation="primary"
-        $size="largebtn"
-        onClick={() => setIsOpenModal((show) => !show)}
-      >
-        Add new cabin
-      </Button>
-
-      {isOpenModal && (
-        <Modal onClose={() => setIsOpenModal(false)}>
-          <CreateCabinForm onCloseModal={() => setIsOpenModal(false)} />
-        </Modal>
-      )}
-    </div>
+    <Model>
+      <Model.Open>
+        <Button>Add new cabin</Button>
+      </Model.Open>
+      <Modal.Window>
+        <CreateCabinForm />
+      </Modal.Window>
+    </Model>
   );
 }
 
+// function AddCabin() {
+//   const [isOpenModal, setIsOpenModal] = useState(false);
+
+//   return (
+//     <div>
+//       <Button
+//         $variation="primary"
+//         $size="largebtn"
+//         onClick={() => setIsOpenModal((show) => !show)}
+//       >
+//         Add new cabin
+//       </Button>
+
+//       {isOpenModal && (
+//         <Modal onClose={() => setIsOpenModal(false)}>
+//           <CreateCabinForm onCloseModal={() => setIsOpenModal(false)} />
+//         </Modal>
+//       )}
+//     </div>
+//   );
+// }
+
 export default AddCabin;
+
+
