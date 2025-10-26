e8385f8eace8faef8b01828f30ba5f6d2fb1bd99
Abdulai059 <abdulaiosman8080@gmail.com>
2025-10-26 15:46:50 +0000

    Update booking filters and status labels

--- src/features/bookings/useBookings.js
+++ src/features/bookings/useBookings.js
@@ -1,18 +1,29 @@
 import { useQuery } from "@tanstack/react-query";
 import { getBookings } from "../../services/apiBookings";
-
+import { useSearchParams } from "react-router-dom";
 
 export function useBookings() {
+  const [searchParams] = useSearchParams();
+
+  //FILTER
+  const filterValue = searchParams.get("status");
+  const filter =
+    !filterValue || filterValue === "all"
+      ? null
+      : { field: "status", value: filterValue };
+  // { field: "totalPrice", value: 5000, method: "gte" };
+
   const {
     isLoading,
     data: bookings,
     error,
   } = useQuery({
-    queryKey: ["bookings"],
-    queryFn: getBookings,
+    queryKey: ["bookings", filter],
+    queryFn: () => getBookings({ filter }),
+
+    // queryKey: ["bookings"],
+    // queryFn: getBookings,
   });
 
   return { isLoading, error, bookings };
 }
-
-
