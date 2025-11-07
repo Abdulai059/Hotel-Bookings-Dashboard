ea785b12befe1f0cd33471a80cc6fcd06f1122eb
Abdulai059 <abdulaiosman8080@gmail.com>
2025-11-07 17:15:27 +0000

    feat: add sales line chart and duration pie chart to dashboard

--- src/features/dashboard/DashboardLayout.jsx
+++ src/features/dashboard/DashboardLayout.jsx
@@ -4,6 +4,8 @@ import Spinner from "../../ui/Spinner";
 import useRecentStays from "./useRecentStays";
 import Stats from "./Stats";
 import { useCabins } from "../cabins/useCabin";
+import SalesChart from "./SalesChart";
+import DurationChart from "./DurationChart";
 
 const StyledDashboardLayout = styled.div`
   display: grid;
@@ -36,8 +38,9 @@ function DashboardLayout() {
         cabinCount={cabins.length}
       />
       <div>Today&apos;s activities</div>
-      <div>Chart stay durations</div>
-      <div>Chart sales</div>
+
+      <DurationChart confirmedStays={confirmedStays} />
+      <SalesChart bookings={bookings} numDays={numDays} />
     </StyledDashboardLayout>
   );
 }
